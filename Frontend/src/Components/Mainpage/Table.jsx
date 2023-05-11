edc186ce12002b79af49c27502514465309f1f74
dineshraj100 <dineshrajmech01@gmail.com>
2023-05-11 16:24:09 +0530

    small changes

--- Frontend/src/Components/Mainpage/Table.jsx
+++ Frontend/src/Components/Mainpage/Table.jsx
@@ -46,7 +46,7 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 
 export function AllocationTableComp() {
   const APIFront = 'https://heartfelt-genie-f4120b.netlify.app';
-  const APIFrontShow = 'https://heartfelt-genie-f4120b.netlify.app';
+  const APIFrontShow = 'heartfelt-genie-f4120b.netlify.app';
   const tableData = useSelector((state) => state.urls.table);
   const dispatch = useDispatch();
   const navigate = useNavigate();
